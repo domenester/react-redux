3345d3e78e4ccf6d8514099071d3841c2d42340e
Diogo Domene <diogo.domene@netlolo.com>
2018-08-15 17:53:28 -0300

    Saving Progress

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,15 +3,17 @@ import * as React from "react";
 export class Main extends React.Component<any, any> {
 
   private name: Array<string> = ["Hello", "World!"];
+  public state: any;
 
   constructor(props: any, context?: any){
     super(props, context);
+    this.state = { content: props.content }
   }
 
   public render() {
       return (
         <div>
-          Main Content. { ...this.name }
+          {this.state.content} { ...this.name }
         </div>
       );   
   }
