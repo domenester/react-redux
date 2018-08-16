38656d7b421b90d8840eca9e30707780f3b47d6a
Diogo Domene <diogo.domene@netlolo.com>
2018-08-16 16:42:57 -0300

    Saving Progress

--- src/components/counter/counter.jsx
+++ src/components/counter/counter.jsx
@@ -3,17 +3,15 @@ import { increment, decrement, stepChanged } from "./counterActions";
 import { bindActionCreators } from "redux"
 import { connect } from "react-redux"
 
-const Counter = props => {
-  console.log('props', props);
-  return(
-    <div>
-      <h1> {props.counter.number} </h1>
-      <input onChange={props.stepChanged} value={props.counter.step} type="number"/>
-      <button onClick={props.increment}>Increment</button>
-      <button onClick={props.decrement}>Decrement</button>
-    </div>
-  )
-}
+const Counter = props => (
+  <div>
+    <h1> {props.counter.number} </h1>
+    <input onChange={props.stepChanged} value={props.counter.step} type="number"/>
+    <button onClick={props.increment}>Increment</button>
+    <button onClick={props.decrement}>Decrement</button>
+  </div>
+)
+
 const mapStateToProps = state => ({counter: state.counter})
 const mapDispatchToProps = dispatch => bindActionCreators({increment, decrement, stepChanged}, dispatch)
 
